c59be33c7b8ca8b4e783cde9f389e1335c53bd90
Armin0001 <59747818+Armin0001@users.noreply.github.com>
2022-11-19 06:32:03 +0100

    initial commit

---
+++ src/components/individual-comment.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable prettier/prettier -->
+
+<template>
+  <div>
+    <li>
+      <img class="post-img" :src="commentpost.authorImg" />
+      <small>{{ commentpost.author }}</small>
+      <p class="post-comment">"{{ commentpost.text }}"</p>
+    </li>
+  </div>
+</template>
+<!-- eslint-disable prettier/prettier -->
+<script>
+export default {
+  name: "IndividualComment",
+  template: "#comment-template",
+  props: ["commentpost"],
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,42 @@
+/* eslint-disable prettier/prettier */
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    newComment: '',
+      comments: [
+      { 
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
+      },
+      { 
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
+      },
+      { 
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
+      }
+    ]
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    // eslint-disable-next-line no-unused-vars
+    addComment: (state) => {
+      const newCommentObj = {
+        text: state.newComment,
+        author: 'Magoo',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-skull.jpg'
+      };
+      state.comments.push(newCommentObj);
+      state.newComment = '';
+    }
+  },
+  actions: {
+    
+  },
   modules: {},
 });
